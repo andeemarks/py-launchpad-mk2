f99afb5c43b7daaabad2b889a7534b47598bb3ec
Andy Marks <vampwillow@gmail.com>
2025-06-12 20:24:56 +1000

    Added classes for message types

--- test.py
+++ test.py
@@ -12,44 +12,76 @@ FIRST_PAD = 11
 SYSEX_HEADER = (0, 32, 41, 2, 24)
 BRIGHT_MAX = 63
 
+class BasicMessage(mido.Message):
+    def __init__(self, offset, color=0, channel=CHANNEL):
+        super().__init__(NOTE_ON, note=FIRST_PAD + offset, channel=channel, velocity=color, time=TIME)
+
+class FlashMessage(BasicMessage):
+    def __init__(self, offset, color=0):
+        super().__init__(offset, color, channel=CHANNEL+1)
+
+class PulseMessage(BasicMessage):
+    def __init__(self, offset, color=0):
+        super().__init__(offset, color, channel=CHANNEL+2)
+
+class SysexMessage(mido.Message):
+    def __init__(self):
+        super().__init__(SYSEX, data=SYSEX_HEADER, time=TIME)
+
 def cell_on(lpad, cell_offset, color):
-    lpad.send(mido.Message(NOTE_ON, note=FIRST_PAD + cell_offset, channel=CHANNEL, velocity=color, time=TIME))
+    lpad.send(BasicMessage(cell_offset, color))
 
 def cell_off(lpad, cell_offset):
-    lpad.send(mido.Message(NOTE_ON, note=FIRST_PAD + cell_offset, channel=CHANNEL, velocity=0, time=TIME))
+    lpad.send(BasicMessage(cell_offset))
 
 def cell_flash(lpad, cell_offset, color):
-    lpad.send(mido.Message(NOTE_ON, note=FIRST_PAD + cell_offset, channel=CHANNEL+1, velocity=color, time=TIME))
+    lpad.send(FlashMessage(cell_offset, color))
 
 def cell_pulse(lpad, x, y, color):
-    lpad.send(mido.Message(NOTE_ON, note=FIRST_PAD + coordinate_pair_to_index(x, y), channel=CHANNEL+2, velocity=color, time=TIME))
+    lpad.send(PulseMessage(cell_offset, color))
 
 def coordinate_pair_to_index(x,y): 
     return x + (10 * y)
 
 def clear_grid(lpad):
-    lpad.send(mido.Message(SYSEX, data=SYSEX_HEADER + (14, 0), time=TIME))
+    message = SysexMessage()
+    message.data += (14, 0)
+    lpad.send(message)
 
 def grid_on(lpad, color):
-    lpad.send(mido.Message(SYSEX, data=SYSEX_HEADER + (14, color), time=TIME))
+    message = SysexMessage()
+    message.data += (14, color)
+    lpad.send(message)
 
 def row_on(lpad, row, color):
-    lpad.send(mido.Message(SYSEX, data=SYSEX_HEADER + (13, row, color), time=TIME))
+    message = SysexMessage()
+    message.data += (13, row, color)
+    lpad.send(message)
 
 def col_on(lpad, col, color):
-    lpad.send(mido.Message(SYSEX, data=SYSEX_HEADER + (12, col, color), time=TIME))
+    message = SysexMessage()
+    message.data += (12, col, color)
+    lpad.send(message)
 
 def scroll_text(lpad, text, color):
-    lpad.send(mido.Message(SYSEX, data=SYSEX_HEADER + (20, color, 0) + tuple([ord(ch) for ch in text]), time=TIME))
+    message = SysexMessage()
+    message.data += (20, color, 0) + tuple([ord(ch) for ch in text])
+    lpad.send(message)
 
 def loop_text(lpad, text, color):
-    lpad.send(mido.Message(SYSEX, data=SYSEX_HEADER + (20, color, 1) + tuple([ord(ch) for ch in text]), time=TIME))
+    message = SysexMessage()
+    message.data += (20, color, 1) + tuple([ord(ch) for ch in text])
+    lpad.send(message)
 
 def loop_stop(lpad):
-    lpad.send(mido.Message(SYSEX, data=SYSEX_HEADER + (20, ), time=TIME))
+    message = SysexMessage()
+    message.data += (20, )
+    lpad.send(message)
 
 def cell_rgb(lpad, x, y, r, g, b):
-    lpad.send(mido.Message(SYSEX, data=SYSEX_HEADER + (11, coordinate_pair_to_index(x, y), r, g, b), time=TIME))
+    message = SysexMessage()
+    message.data += (11, coordinate_pair_to_index(x, y), r, g, b)
+    lpad.send(message)
 
 lpad = mido.open_output('Launchpad MK2 MIDI 1')
 
