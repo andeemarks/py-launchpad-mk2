483edb1b1223df289c21bebf18e1f65af2489cee
Andy Marks <vampwillow@gmail.com>
2025-06-12 17:36:37 +1000

    Added cell_off, cell_flash and cell_pulse PoC

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import mido
+import random
 
 NOTE_ON = 'note_on'
 CHANNEL = 0
@@ -6,9 +7,34 @@ VELOCITY = 81
 TIME = 0
 FIRST_PAD = 10
 
+
+def clear_grid(lpad):
+    lpad.send(mido.Message('sysex', data=[240, 0, 32, 41, 2, 24, 14, 247]))
+
+def cell_on(lpad, cell_offset):
+    lpad.send(mido.Message(NOTE_ON, note=FIRST_PAD + cell_offset, channel=CHANNEL, velocity=random.randint(0, 127), time=TIME))
+
+def cell_off(lpad, cell_offset):
+    lpad.send(mido.Message(NOTE_ON, note=FIRST_PAD + cell_offset, channel=CHANNEL, velocity=0, time=TIME))
+
+def cell_flash(lpad, cell_offset):
+    lpad.send(mido.Message(NOTE_ON, note=FIRST_PAD + cell_offset, channel=CHANNEL+1, velocity=random.randint(0, 127), time=TIME))
+
+def cell_pulse(lpad, cell_offset):
+    lpad.send(mido.Message(NOTE_ON, note=FIRST_PAD + cell_offset, channel=CHANNEL+2, velocity=random.randint(0, 127), time=TIME))
+
 lpad = mido.open_output('Launchpad MK2 MIDI 1')
-# lpad.send(mido.Message.from_bytes([0x90, 0x40, 0x60]))
-for (pad_offset, color_offset) in zip(range(0, 9 * 9), range(0, 9 * 9)):
-    lpad.send(mido.Message(NOTE_ON, note=FIRST_PAD + pad_offset, channel=CHANNEL, velocity=color_offset, time=TIME))
+
+for cell_offset in range(0, 9 * 9):
+    cell_on(lpad, cell_offset)
+    
+for cell_offset in range(0, 9 * 9):
+    cell_off(lpad, cell_offset)
+
+for cell_offset in range(0, 9 * 9):
+    cell_flash(lpad, cell_offset)
+
+for cell_offset in range(0, 9 * 9):
+    cell_pulse(lpad, cell_offset)
 
 lpad.close()
